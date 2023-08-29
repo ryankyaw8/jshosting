3d3d942d87bfc33c81e10f390d0adb364d6d3286
Ryan Kyaw <96888698+ryankyaw8@users.noreply.github.com>
2023-08-28 17:05:34 -0700

    Update jgafform.js

--- jgafform.js
+++ jgafform.js
@@ -4,7 +4,7 @@ function trimfield() {
     var valueOne = loader.engine.document.getElementById(106773007).getProperty('value.value');
     var valueTwo = loader.engine.document.getElementById(106773008).getProperty('value.value');
     
-    loader.engine.document.getElementById(106773007).setValue(({"value": valueOne.toLowerCase()}));
+    loader.engine.document.getElementById(106773007).setValue(({"value": valueOne.trim()}));
     loader.engine.document.getElementById(106773008).setValue(({"value": valueTwo.trim()}));
 }
 
